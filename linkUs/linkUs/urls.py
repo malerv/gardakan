cb08e1eacc419f4378a7969f635f907770f6669f
malerv <kevinalb@gmail.com>
2014-03-22 13:41:55 -0400

    ajout de la vue pour le log, et modification de l'url en consequence.

--- linkUs/linkUs/urls.py
+++ linkUs/linkUs/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from chatUs import views
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,6 +9,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'linkUs.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^index.html$',TemplateView.as_view(template_name="index.html"), name='home'),
+    url(r'^index.html$', views.login, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
