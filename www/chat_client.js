2b1515c2c5e06d5cfa62708516331ff5844efd4a
Tommy Duchesne <tommy.duchesne2@usherbrooke.ca>
2014-03-22 18:16:16 -0400

    Added post function to send entered messages, not tested

--- www/chat_client.js
+++ www/chat_client.js
@@ -1,14 +1,24 @@
 function enterMessage(textID)
 {
     var textBox = document.getElementById(textID);
+    var textHF = document.getElementById("textToSend");
+    var currentTimeHF = document.getElementById("currentTime");
+    var sendTextForm = document.getElementById("inputMessageForm");
     var messageText = textBox.value;
     textBox.value = "";
     
-    if(messageText != null && messageText.trim() != "")
+    if(messageText != null && messageText.trim() != "" && messageText.length <= 255)
     {
         console.log("Sending message : " + messageText);
         insertMessage('userName', messageText,'chatMessagesdiv_1'); 
     }
+    else {
+        return;
+    }
+    
+    textHF.value = messageText;
+    currentTimeHF.value = new Date().getTime() / 1000;
+    sendTextForm.submit();
 }
 
 function insertMessage(userID, message, containerID)
