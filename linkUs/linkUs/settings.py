935ecdec2eb21425d0a5916db133f966597c922a
malerv <kevinalb@gmail.com>
2014-03-22 11:52:46 -0400

    ajout du style de base et de la page d'index

--- linkUs/linkUs/settings.py
+++ linkUs/linkUs/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -58,7 +59,7 @@ WSGI_APPLICATION = 'linkUs.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'LinkUsDB'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -79,4 +80,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+STATIC_URL = '/static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATICFILES_FINDERS = ("django.contrib.staticfiles.finders.FileSystemFinder",
+                        "django.contrib.staticfiles.finders.AppDirectoriesFinder")
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_URL = '/static/'
--- linkUs/linkUs/urls.py
+++ linkUs/linkUs/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,6 +8,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'linkUs.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^index.html$',TemplateView.as_view(template_name="index.html"), name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
