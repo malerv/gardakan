a9693c965b4e3f82ff314b68929888a5123d92ef
Tommy Duchesne <tommy.duchesne2@usherbrooke.ca>
2014-03-22 16:10:33 -0400

    Added button, now inputing message display them automatically in the chat as a test

--- www/chat_client.js
+++ www/chat_client.js
@@ -4,5 +4,23 @@ function enterMessage(textID)
     var messageText = textBox.value;
     textBox.value = "";
     
-    console.log(messageText);
+    if(messageText != null && messageText.trim() != "")
+    {
+        console.log("Sending message : " + messageText);
+        insertMessage('userName', messageText,'chatMessagesdiv_1'); 
+    }
+}
+
+function insertMessage(userID, message, containerID)
+{
+    var user = document.getElementById(userID).value;
+    var mycontainer = document.getElementById(containerID);
+    var newMessage = '';
+    
+    newMessage += '<div class="chatmessage">';
+    newMessage += '<strong><span>' + user + ': </span>&nbsp;</strong>';
+    newMessage += '<span>' + message + '</span>';
+    newMessage += '</div>';
+    
+    mycontainer.innerHTML += newMessage;
 }
