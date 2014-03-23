61d4dcf08f3e2d5281316b2a28cb868cf329cc1c
YourTimeIsMine <coolvincent120@hotmail.com>
2014-03-23 03:04:01 -0400

    update_10

--- linkUs/chatUs/views.py
+++ linkUs/chatUs/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django import forms
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from chatUs.models import Event
+from utils.geo import getLatFromStrPoint, getLonFromStrPoint
 # Create your views here.
 
 class LoginForm(forms.Form):
@@ -10,6 +12,18 @@ class LoginForm(forms.Form):
     """
     username = forms.CharField()
 
+class CreateEventForm(forms.Form):
+    Name = forms.CharField()
+    StartDate = forms.DateField()
+    EndDate = forms.DateField()
+    StartTime = forms.TimeField()
+    EndTime = forms.TimeField()
+    Cost = forms.DecimalField()
+    Phone = forms.CharField()
+    Email = forms.CharField()
+    Description = forms.CharField()
+    City = forms.CharField()
+    Adress = forms.CharField()
 
 def login(request):
     """
@@ -46,4 +60,37 @@ def event_list(request):
     return render(request, 'eventList.html', {})
 
 def create_event(request):
-    return render(request, 'createEvent.html', {})
+    if request.method == 'POST': # If the form has been submitted...
+        form = CreateEventForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            
+            print request.POST
+            if request.POST['ville'].tolower() == "sherbrooke":
+                
+                
+                DjanCity, fuckOff = City.objects.get_or_create(Name = "Sherbrooke")
+                DjanEvent = Event(
+                                  Name = request.POST['titre'],
+                                  StartDate = request.POST['dateDebut'],
+                                  EndDate = request.POST['dateFin'],
+                                  StartTime = request.POST['heureDebut'],
+                                  EndTime = request.POST['heureFin'],
+                                  Cost = request.POST['cout'],
+                                  Phone = request.POST['telephone'],
+                                  Email = request.POST['email'],
+                                  Description = request.POST['descript'],
+                                  Adress = request.POST['address'],
+                                  Latitude = getLatFromStrPoint(request.POST['address']),
+                                  Longitude = getLonFromStrPoint(request.POST['address'])
+                                  )
+                DjanEvent.save()
+                
+            # Process the data in form.cleaned_data
+            # ...
+           # return HttpResponseRedirect('/thanks/') # Redirect after POST
+        else:
+            form = CreateEventForm() # An unbound form
+
+    return render(request, 'createEvent.html', {
+        'form': form,
+    })
