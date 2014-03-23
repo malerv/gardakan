9b8a2f152ba0b96c88474fa1fa7bf0de5e5d7949
YourTimeIsMine <coolvincent120@hotmail.com>
2014-03-22 21:31:32 -0400

    update_3

--- linkUs/chatUs/Util_DB.py
+++ linkUs/chatUs/Util_DB.py
@@ -8,7 +8,7 @@ import json
 import math
 from pprint import pprint
 from chatUs.models import Event, City
-from utils.geo  import  getLatFromStrPoint, getLonFromStrPoint
+from utils.geo  import  getLatFromStrPoint, getLonFromStrPoint, getDistanceBetween
 
 #from utils  import geo
 from operator import itemgetter
@@ -19,38 +19,69 @@ def AddEvents(events):
     for event in events:
         pprint(event)
         
-        DjanCity, fuckOff = City.objects.get_or_create(Name="Sherbrooke",MunID=event['MUNID'])
         
+     
+        tName  = event.get('TITRE')
+        tSubtitle = None
+        tDescription = event.get('DESCRIP')
+        tInformation = None
+        tStartDate = event.get('DT01')
+        tEndDate = event.get('DT02')
+        tStartTime = event.get('HR01')
+        tEndTime = event.get('HR02')
+        tCategory = event.get('CATEG')
+        tAdress = event.get('AD')
+        tLocation = event.get('LOC')
+        tLatitude = 0
+        tLongitude = 0
+        tCityName = "Sherbrooke"
+        tMunID = event.get('MUNID')
+        tGeometry = event.get('GEOM')
+        tWeekDay = event.get('JOUR_SEMAINE')
+    
         
+        if tAdress == None:
+            
+            if tLocation != None:
+                tAdress = tLocation
+            else:
+                tAdress = None
+       
+             
+        if tGeometry == None:
         
-        if event.get('AD') == None:
-            Ad = "PAS_DADRESSE"
-           # Ad = TODO_GetPointFromAdress( MUN + LOC)
+            if tAdress != None:
+                pass
+                #TODO_GetPointFromAdress(Ad)  
+            else:
+                tLatitude = 0
+                tLongitude = 0
+                 
         else:
-            Ad = event['AD']
-        
+            
+            tLatitude = getLatFromStrPoint(tGeometry)
+            tLongitude = getLonFromStrPoint(tGeometry)
+            
 
-        
-        if event.get('GEOM') == None:
-            Lat = 0
-            Lon = 0
-            #TODO_GetPointFromAdress
-        else:
-            Lat = getLatFromStrPoint(event['GEOM'])
-            Lon = getLonFromStrPoint(event['GEOM'])
-            
-            
-        
-        
+        DjanCity, fuckOff = City.objects.get_or_create(Name = tCityName, MunID = tMunID)
+
+
         DjanEvent = Event(
-                          Name=event['TITRE'],
-                          Description=event['DESCRIP'],
-                          DateTime=event['DT01'],
-                          Category=event['CATEG'],
-                          Adress=Ad,
-                          Latitude=Lat,
-                          Longitude=Lon,
-                          City = DjanCity )
+                          Name = tName,
+                          Subtitle = tSubtitle,
+                          Description = tDescription,
+                          Information = tInformation,
+                          StartDate = tStartDate,
+                          EndDate = tEndDate,
+                          StartTime = tStartTime,
+                          EndTime = tEndTime,
+                          WeekDay = tWeekDay,
+                          Category = tCategory,
+                          Adress = tAdress,
+                          Latitude = tLatitude,
+                          Longitude = tLongitude,
+                          City = DjanCity
+                           )
         
         DjanEvent.save()
     
@@ -60,20 +91,20 @@ def AddEvents(events):
 def AddLibraryEvents():
     json_data=open('../DonneeOuverte/SherbrookeLibraryEvents.json')
     data = json.load(json_data)
-    AddEvents(data)
+    AddEvents(data['EVTS']['EVT'])
         
     
 
 def AddHockeyEvents():
     json_data=open('../DonneeOuverte/SherbrookeHockeyEvents.json')
     data = json.load(json_data)
-    AddEvents(data)
+    AddEvents(data['PATIN_HOCKEY_LIBRES']['PATIN_HOCKEY_LIBRE'])
        
        
 def AddPoolEvents():  
     json_data=open('../DonneeOuverte/SherbrookePoolEvents.json')
     data = json.load(json_data)
-    AddEvents(data)
+    AddEvents(data['EVTS']['EVT'])
     
 def AddSherbrookeEvents():
     json_data=open('../DonneeOuverte/SherbrookeEvents.json')
@@ -85,17 +116,26 @@ def FillDB():
     print "DEBUT"
     AddSherbrookeEvents()
    # AddLibraryEvents()
-   # AddHockeyEvents()
-   # AddPoolEvents()
+    AddHockeyEvents()
+    #AddPoolEvents()
     print "FIN"
     
+    
+
     
 def GetSortedEventList(UserLat,UserLon):
     DjanEvents = Event.objects.all()
     array = []
     
     for DjanEvent in DjanEvents:
+        
+       # print DjanEvent.Latitude
+        #print DjanEvent.Longitude
+        #print UserLat
+        #print UserLon
+        
         dist = getDistanceBetween(DjanEvent.Latitude, DjanEvent.Longitude, UserLat, UserLon)
+        print dist
         array.append([DjanEvent,dist])
         
     SortedList = sorted(array, key=itemgetter(1))
@@ -107,8 +147,8 @@ def GetSortedEventList(UserLat,UserLon):
 def main():
     # my code here
     print "Main"
-    #FillDB()
-    GetSortedEventList(5,5)
+    FillDB()
+    GetSortedEventList(45.399896,-71.884232)
     
 main()
 
