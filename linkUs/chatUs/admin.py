7b974915df222573f2f440f8a54e03df1a91ff72
malerv <kevinalb@gmail.com>
2014-04-01 22:22:56 -0400

    add the event in the admin interface

--- linkUs/chatUs/admin.py
+++ linkUs/chatUs/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from .models import ChatLOG
+from .models import ChatLOG, Event
 
 # Register your models here.
 
 class ChatLOGAdmin(admin.ModelAdmin):
     pass
 
+class EventAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(ChatLOG, ChatLOGAdmin)
+admin.site.register(Event, EventAdmin)
